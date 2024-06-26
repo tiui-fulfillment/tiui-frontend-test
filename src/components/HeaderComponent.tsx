74585fab6a947f157f547a3d873b735c4445acd1
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-06-26 17:00:11 -0600

    :sparkles: Add HeaderComponent

---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,8 @@
+export default function HeaderComponent() {
+  return (
+    <div className="flex justify-between items-center">
+      <h2 className="text-2xl font-bold">Tareas</h2>
+      <span className="text-teal-600 font-semibold">Tiui mx</span>
+    </div>
+  );
+}
