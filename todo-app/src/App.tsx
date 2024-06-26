e3d876f12c201b6f3152fea799d7c4b8e6a7869c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-06-26 12:56:28 -0600

    feat: inicializar proyecto

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-vw-100 min-vh-100 d-flex justify-content-center align-items-center">
+      <h1>Todo App</h1>
+
+      <button type="button" className="btn btn-primary">Agregar tarea</button>
+    </div>
+  )
+}
+
+export default App
