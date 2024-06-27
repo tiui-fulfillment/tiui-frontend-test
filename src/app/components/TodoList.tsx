b3a40e4835f3ec00f3ba1863d55348429e22239d
Luis Garcia <luisg.garciaespinoza@gmail.com>
2024-06-26 19:38:42 -0600

    added TodoList component

---
+++ src/app/components/TodoList.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { useState } from 'react';
+
+const TodoList = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Lista de tareas Luis García</h1>
+    </div>
+  );
+};
+
+export default TodoList;
