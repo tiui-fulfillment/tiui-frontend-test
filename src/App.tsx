719d616b55bf0da3cd8431200fcba39d80e20aec
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-06-26 19:11:50 -0600

    :sparkles: Add Task logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import DialogComponent from './components/DialogComponent';
+import FilterComponent from './components/FilterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import { TaskComponent } from './components/TaskComponent';
+import { Task } from './types/types';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#0d9488',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: uuidv4(),
+      title: 'UI work',
+      description: 'Terminar la interfaz de las tareas',
+      completed: false,
+    },
+  ]);
+
+  const [filteredTasks, setFilteredTasks] = useState<Task[]>(tasks);
+
+  useEffect(() => {
+    handleFilter('Todas');
+  }, []);
+
+  const handleFilter = (filter: string) => {
+    if (filter === 'Todas') {
+      setFilteredTasks(tasks);
+    } else if (filter === 'Completadas') {
+      setFilteredTasks(tasks.filter((task) => task.completed));
+    } else if (filter === 'Sin completar') {
+      setFilteredTasks(tasks.filter((task) => !task.completed));
+    }
+  };
+
+  const addTask = (task: Task) => {
+    const updatedTasks = [...tasks, { ...task, id: uuidv4() }];
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (id: string) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const toggleTask = (id: string) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const editTask = (editedTask: Task) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === editedTask.id) {
+        return editedTask;
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  useEffect(() => {
+    handleFilter('Todas');
+  }, [tasks]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="min-h-[100vh] w-full p-8 grid md:place-items-center bg-[#0F0F0F] text-neutral-200">
+        <div className="bg-neutral-900 h-fit rounded-xl p-6 flex flex-col gap-4 w-full xl:w-4/6 md:w-fit md:min-w-96">
+          <HeaderComponent />
+          <FilterComponent handleFilter={handleFilter} />
+          <div className="h-72 overflow-y-auto flex flex-col gap-4">
+            {filteredTasks.map((task) => (
+              <TaskComponent
+                key={task.id}
+                task={task}
+                deleteTask={deleteTask}
+                toggleTask={toggleTask}
+                editTask={editTask}
+              />
+            ))}
+          </div>
+          <DialogComponent addTask={addTask} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
