82c81355964fc43c1a7a3230e69b398aed40cdb5
Luis Garcia <luisg.garciaespinoza@gmail.com>
2024-06-26 19:32:54 -0600

    added bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.css';
 import "./globals.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
