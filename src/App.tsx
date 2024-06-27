f685be8de98f60578245f6d0c57d7cd72cf78925
AlexMaderaP <alejandro.maderap@gmail.com>
2024-06-27 13:04:13 -0600

    Add:main structure of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import TodoList from "./Components/TodoList";
 
 function App() {
-  return <Typography variant="h1">EasyTask</Typography>;
+  return (
+    <>
+      <nav>
+        <Typography variant="h1">EasyTask</Typography>
+        <Button variant="contained">New ToDo</Button>
+      </nav>
+      <main>
+        <Typography variant="h3">To Do List</Typography>
+        <TodoList />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type ToDo = {
+  id: string;
+  name: string;
+  completed: boolean;
+};
