deae69c152268186572e6f3452706233de805c0d
AlexMaderaP <alejandro.maderap@gmail.com>
2024-06-28 09:30:48 -0600

    Add: edit todo feature

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -6,9 +6,11 @@ import {
   CardContent,
   CardActions,
   Box,
+  TextField,
 } from "@mui/material";
 import { useTodos } from "../hooks/useTodos";
 import { Todo } from "../types";
+import { useState } from "react";
 
 export default function TodoList() {
   const { todos } = useTodos();
@@ -24,6 +26,30 @@ export default function TodoList() {
 }
 
 function TodoCard({ todo }: { todo: Todo }) {
+  const { editTodo } = useTodos();
+  const [isComplete, setIsComplete] = useState(todo.completed);
+  const [isEditing, setIsEditing] = useState(false);
+  const [description, setDescription] = useState(todo.description);
+
+  function handleCompleteChange() {
+    editTodo({ ...todo, completed: !isComplete });
+    setIsComplete((prev) => !prev);
+  }
+
+  function handleEditing() {
+    if (isEditing) {
+      const updatedTodo = { ...todo, description: description };
+      editTodo(updatedTodo);
+    }
+    setIsEditing((prev) => !prev);
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent) {
+    if (e.key === "Enter") {
+      handleEditing();
+    }
+  }
+
   return (
     <Card
       sx={{
@@ -32,15 +58,42 @@ function TodoCard({ todo }: { todo: Todo }) {
         justifyContent: "space-between",
         p: 1,
         mb: 1,
+        opacity: isComplete ? 0.7 : 1,
       }}
     >
-      <CardContent sx={{ display: "flex", alignItems: "center" }}>
-        <Checkbox checked={todo.completed} />
-        <Typography variant="body1">{todo.description}</Typography>
+      <CardContent
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          textDecoration: isComplete ? "line-through" : "none",
+        }}
+      >
+        <Checkbox
+          checked={isComplete}
+          onChange={handleCompleteChange}
+          disabled={isEditing}
+        />
+        {isEditing ? (
+          <TextField
+            autoFocus
+            value={description}
+            variant="standard"
+            onChange={(e) => setDescription(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        ) : (
+          <Typography variant="body1">{todo.description}</Typography>
+        )}
       </CardContent>
       <CardActions>
-        <Button variant="outlined" color="primary" sx={{ mr: 1 }}>
-          Edit
+        <Button
+          variant="outlined"
+          color="primary"
+          sx={{ mr: 1 }}
+          disabled={isComplete}
+          onClick={handleEditing}
+        >
+          {isEditing ? "Save" : "Edit"}
         </Button>
         <Button variant="outlined" color="warning">
           Delete
