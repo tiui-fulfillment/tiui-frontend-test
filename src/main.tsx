6fb2a6d04504032b1ce703795d22d8c1e66b29fe
jesusrobot0 <jesusrobot9000@icloud.com>
2024-06-29 11:28:42 -0500

    Añade Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { muiTheme } from "./config/mui-theme";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={muiTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
